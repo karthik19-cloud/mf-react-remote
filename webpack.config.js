69f7d87321e6d793f0e5c72f029410d7ce7f15fa
karthik19-cloud <32713545+karthik19-cloud@users.noreply.github.com>
2022-11-22 19:48:42 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const deps = require("./package.json").dependencies;
 module.exports = {
   output: {
-    publicPath: "http://localhost:8081/",
+    publicPath: "https://master.d1494ug9d7lo2o.amplifyapp.com/",
   },
 
   resolve: {
